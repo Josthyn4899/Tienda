82e25bdda9692524ddb9b48fecdca6ad5b6293d3
Josthyn4899 <josthyn4899@gmail.com>
2025-02-18 21:10:14 -0600

    Semana 06

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -15,15 +15,34 @@ public class CategoriaServiceImpl implements CategoriaService{
 
     @Autowired
     private CategoriaDao categoriaDao;
-    
-    public List<Categoria> getCategorias(boolean activos){
-            var lista = categoriaDao.findAll();
-            
-            if(activos){
-            lista.removeIf(c -> !c.isActivo());
-            
-            }
-            return lista;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }
-}
+
     
